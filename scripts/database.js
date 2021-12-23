97d6106e7d5d73f51f87d04dfae72f8e671baffa
claudiuilie <claudiu.ilie0322@gmail.com>
2021-12-23 14:55:31 +0200

    'scripts'

--- scripts/database.js
+++ scripts/database.js
@@ -2,8 +2,8 @@ const pool = require("../config/mySql");
 const helper = require("../helpers/dbHelper");
 const shell = require('shelljs')
 
-const code = shell.exec('python3 read_fan_speed.py').code;
-shell.echo(code);
+const code = shell.exec('mysql -u root -e "create database testdb";').code;
+console.log(code);
 // shell.exec('exit');
 // console.log(process.env.DB_APP_USER);
 //
