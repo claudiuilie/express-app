48cd0894882a51b27a9aaccd7b586b5aa23318e0
claudiuilie <claudiu.ilie0322@gmail.com>
2021-12-23 15:47:54 +0200

    'scripts'

--- scripts/database.js
+++ scripts/database.js
@@ -1,6 +1,6 @@
 const pool = require("../config/mySql");
 const helper = require("../helpers/dbHelper");
-const shell = require('shelljs')
+const shell = require('shelljs');
 
 // const code = shell.exec('mysql -u root -e "create database testdb";').code;
 // console.log(code);
@@ -14,26 +14,25 @@ function exec(command) {
     }));
 }
 
-async function runScript(script){
+async function runScript(script) {
     shell.echo('##################################################');
-    shell.echo(`Execute script: ${script}`)
+    shell.echo(`Execute script: ${script}`);
     const process = await shell.exec(script);
-    if(process.code !== 0){
+    if (process.code !== 0) {
         shell.exit(1);
     }
-    shell.echo({
-        code: process.code,
-        message: process.stdout
-    });
-};
+    shell.echo(`
+        code: ${process.code}
+        message: ${process.stdout}`);
+}
 
-async function check(){
+async function check() {
     shell.echo('Start database script...');
     await runScript('mysql -u root -e "create database testdb";');
     shell.echo('Database script ended successfully.');
 }
 
-check()
+check();
 
 // shell.exec('exit');
 // console.log(process.env.DB_APP_USER);
