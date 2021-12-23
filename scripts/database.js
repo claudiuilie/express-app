32887a23c2df7fc6957d78c59cde43cf3c52c113
claudiuilie <claudiu.ilie0322@gmail.com>
2021-12-23 18:40:38 +0200

    'scripts'

--- scripts/database.js
+++ scripts/database.js
@@ -20,6 +20,10 @@ DB_LIMIT_QUEUE=0
 DB_LIMIT_LIST_PER_PAGE=10
 DB_WAIT_FOR_CONNECTIONS=true`;
 
+function escapeShell(cmd) {
+    return '"'+cmd.replace(/(["'$`\\])/g,'\\$1')+'"';
+};
+
 function getDbName(param) {
     const keys = Object.keys(param);
     for (let key in keys) {
@@ -62,15 +66,15 @@ function updateEnvConfig(content) {
     shell.echo('Start database script...');
     shell.echo(`Parameters: ${JSON.stringify(params)}`);
 
-    await exec(`mysql -u root -e "CREATE DATABASE ${params.DB_NAME}";`);
-    await exec(`mysql -u root -e "CREATE USER '${params.DB_APP_USER}'@'localhost' IDENTIFIED BY '${params.DB_APP_USER_PW}';
-            GRANT ALL PRIVILEGES ON ${params.DB_NAME}.* TO '${params.DB_APP_USER}'@'localhost';
-            FLUSH PRIVILEGES;"`);
-    await exec(`mysql -u root -e "CREATE USER '${params.DB_ADMIN}'@'%' IDENTIFIED BY '${params.DB_ADMIN_PW}';
-            GRANT ALL PRIVILEGES ON ${params.DB_NAME}.* TO '${params.DB_ADMIN}'@'%';
-            FLUSH PRIVILEGES;"`);
-    await exec(`mysql -u root -e "CREATE TABLE ${params.DB_NAME}.users (id int(11) NOT NULL AUTO_INCREMENT,username varchar(100) NOT NULL,password varchar(100) NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;"`);
-    await exec(`mysql -u root -e "INSERT INTO ${params.DB_NAME}.users (id, username, password) VALUES(null, 'test', '\$2b\$10\$7l9mAr5UytdZrKFw8iBF7.Uf4pCH0wA2TAlNDDjcnl0HzvxEt2hje');"`);
+    // await exec(`mysql -u root -e "CREATE DATABASE ${params.DB_NAME}";`);
+    // await exec(`mysql -u root -e "CREATE USER '${params.DB_APP_USER}'@'localhost' IDENTIFIED BY '${params.DB_APP_USER_PW}';
+    //         GRANT ALL PRIVILEGES ON ${params.DB_NAME}.* TO '${params.DB_APP_USER}'@'localhost';
+    //         FLUSH PRIVILEGES;"`);
+    // await exec(`mysql -u root -e "CREATE USER '${params.DB_ADMIN}'@'%' IDENTIFIED BY '${params.DB_ADMIN_PW}';
+    //         GRANT ALL PRIVILEGES ON ${params.DB_NAME}.* TO '${params.DB_ADMIN}'@'%';
+    //         FLUSH PRIVILEGES;"`);
+    // await exec(`mysql -u root -e "CREATE TABLE ${params.DB_NAME}.users (id int(11) NOT NULL AUTO_INCREMENT,username varchar(100) NOT NULL,password varchar(100) NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;"`);
+    await exec(`mysql -u root -e "INSERT INTO ${params.DB_NAME}.users (id, username, password) VALUES(null, 'test', '${escapeShell('$2b$10$7l9mAr5UytdZrKFw8iBF7.Uf4pCH0wA2TAlNDDjcnl0HzvxEt2hje')}');"`);
 
     shell.echo(`Ui test user created :test@test`);
     shell.echo('\nDatabase script ended successfully.');
