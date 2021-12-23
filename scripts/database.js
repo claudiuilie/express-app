449cc8f69b6ba591163e169b440c9b8d4cb61454
claudiuilie <claudiu.ilie0322@gmail.com>
2021-12-23 14:49:57 +0200

    'scripts'

--- scripts/database.js
+++ scripts/database.js
@@ -1,2 +1,19 @@
-console.log(process.env.DB_APP_USER);
-console.log(process.env.NODE_ENV);
+const pool = require("../config/mySql");
+const helper = require("../helpers/dbHelper");
+const shell = require('shelljs')
+
+shell.echo('Test')
+// console.log(process.env.DB_APP_USER);
+//
+// async function createUsers() {
+//
+//
+//     const rows = await pool.query(
+//         `CREATE USER 'app_admin'@'%' IDENTIFIED BY 'BscPlatformAdmin2021';
+//              GRANT ALL PRIVILEGES ON bsc_platform.* TO 'app_admin'@'%';
+//              FLUSH PRIVILEGES;`,
+//         [offset]
+//     );
+//     const data = helper.emptyOrRows(rows);
+//     console.log(data);
+// };
