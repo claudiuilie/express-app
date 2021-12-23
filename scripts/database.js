3a2a2fd3bdf3aff441df6c28bfd53f567946756a
claudiuilie <claudiu.ilie0322@gmail.com>
2021-12-23 14:53:56 +0200

    'scripts'

--- scripts/database.js
+++ scripts/database.js
@@ -2,9 +2,9 @@ const pool = require("../config/mySql");
 const helper = require("../helpers/dbHelper");
 const shell = require('shelljs')
 
-const code = shell.exec('sudo mysql').code;
-console.log(code);
-shell.exec('exit');
+const code = shell.exec('').code;
+shell.echo(code);
+// shell.exec('exit');
 // console.log(process.env.DB_APP_USER);
 //
 // async function createUsers() {
