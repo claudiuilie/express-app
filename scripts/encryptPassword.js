a86fa36bd79355bc697c444b1eae4c8e5595b5d6
claudiuilie <claudiu.ilie0322@gmail.com>
2022-01-07 17:38:11 +0200

    encript password script

--- scripts/encryptPassword.js
+++ scripts/encryptPassword.js
@@ -2,6 +2,9 @@ const encryptionUtils = require('../utils/encryptionUtils');
 const password = process.env.PASSWORD;
 
 (async function encrypt(){
+    if (password === undefined || password === '') {
+        throw new Error(`PASSWORD is undefined!`)
+    }
     const encrypted = await encryptionUtils.encrypt(password);
     console.log(encrypted);
 })();
